96f8d93858aa95233868e3c061d81ab7558977ae
Garrett Denham <g.w.denham@gmail.com>
2024-09-08 02:43:59 -0400

    Creating custom keymap

---
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/custom/rules.mk
@@ -0,0 +1,10 @@
+ENCODER_MAP_ENABLE = yes
+
+LTO_ENABLE = yes
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+NKRO_ENABLE = yes           # Enable N-Key Rollover
+RGB_MATRIX_ENABLE = yes
+IDLE_TIMEOUT_ENABLE = yes
+VIA_ENABLE = yes
+
+SIGNALRGB_SUPPORT_ENABLE = yes
